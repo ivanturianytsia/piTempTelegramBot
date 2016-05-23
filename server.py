6e3509c182472cfc580e227a77bed1cd2f0237a2
sirivan <vanyaturianitsa@gmail.com>
2016-05-23 17:36:33 +0200

    Termometer.measure returns random number, added degree sign

--- server.py
+++ server.py
@@ -2,22 +2,25 @@
 # This bot was made specifically for the pyTelegramAPI Telegram chat,
 # and goes by the name 'TeleBot (@pyTeleBot)'. Join our group to talk to him!
 # WARNING: Tested with Python 2.7
-
 import telebot
+from random import randint
 import os
 
 class Termometer(object):
 	def __init__(self):
 		self.temperature = 0
-	def measureTemperature(self):
+	def measure(self):
 		# Get data from sensor here
-		# self.temperature = newTemp
-		return self.temperature
-	def getTemp(self):
+		self.temperature = randint(-20, 45)
+
+	def get(self):
+		self.measure()
 		return str(self.temperature)
 
-bot = telebot.AsyncTeleBot("230403950:AAHHFSPqeR4c9EdH0BuvJCFXVin7P0B-8Ck")
 t = Termometer()
+
+
+bot = telebot.AsyncTeleBot("230403950:AAHHFSPqeR4c9EdH0BuvJCFXVin7P0B-8Ck")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -26,7 +29,7 @@ def send_welcome(message):
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	if message.text.lower() == "temp":
-		bot.send_message(message.from_user.id, "Currently temperature is: " + t.getTemp())
+		bot.send_message(message.from_user.id, "Currently temperature is: " + t.get() + u'\N{DEGREE SIGN}' + "C.")
 	else:
 	    bot.send_message(message.from_user.id, "Send 'temp' to get current temperature.")
 
