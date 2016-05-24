09c9decf2b9809ef84061fece05934da225070a3
sirivan <vanyaturianitsa@gmail.com>
2016-05-24 12:16:00 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -16,17 +16,6 @@ gulp.task('default', function() {
         })
         .on('change', function(newHash, oldHash) {
             console.log('Git CHANGES! FROM', oldHash, '->', newHash);
-            if (shell) {
-                console.log("\t[Pyhon shell]: ending old...");
-                shell.end(function(err) {
-                    if (err) {
-                        console.log("\t[Pyhon shell]: " + err)
-                    } else {
-                        console.log("\t[Pyhon shell]: old ended.");
-
-                    }
-                })
-            }
             startPython()
         });
 });
