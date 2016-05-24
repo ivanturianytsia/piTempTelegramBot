cbf9ccd5a9ccf802d30f9410f570b9f610e50060
sirivan <vanyaturianitsa@gmail.com>
2016-05-24 11:59:18 +0200

    starting

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gitWatch = require('gulp-git-watch');
 var PythonShell = require('python-shell');
 var shell;
 var startPython = function() {
+    console.log("Pyhon shell: starting new...");
     shell = new PythonShell('server.py')
     console.log("Pyhon shell: New started");
 }
@@ -16,6 +17,7 @@ gulp.task('default', function() {
         .on('change', function(newHash, oldHash) {
             console.log('Git CHANGES! FROM', oldHash, '->', newHash);
             if (shell) {
+                console.log("Pyhon shell: ending old...");
                 shell.end(function() {
                     console.log("Pyhon shell: Old ended");
                     startPython()
